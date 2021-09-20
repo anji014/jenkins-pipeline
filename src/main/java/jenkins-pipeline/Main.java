e2656a0ca2ac3d47f6714dd682eace873503144c
anji014 <82080207+anji014@users.noreply.github.com>
2021-09-20 12:33:52 +0530

    Update Main.java

--- src/main/java/jenkins-pipeline/Main.java
+++ src/main/java/jenkins-pipeline/Main.java
@@ -1,13 +1,18 @@
 package jenkins-pipeline;
 
+import java.util.Scanner;  // Import the Scanner class
+
 public class Main {
+  public static void main(String[] args) {
+    int x, y, sum;
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Type a number:");
+    x = myObj.nextInt(); // Read user input
 
-    public static void main(String[] args) {
+    System.out.println("Type another number:");
+    y = myObj.nextInt(); // Read user input
 
-        int num = 5;
-        for(int i = 1; i <= 10; ++i)
-        {
-            System.out.printf("%d * %d = %d \n", num, i, num * i);
-        }
-    }
-}
+    sum = x + y;
+    System.out.println("Sum is: " + sum); // Output user input
+  }
+} 
