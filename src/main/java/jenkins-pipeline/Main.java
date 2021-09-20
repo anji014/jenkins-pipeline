ea49ab7515bd821357180a3bcf88acf0178e74a4
anji014 <82080207+anji014@users.noreply.github.com>
2021-09-20 12:26:08 +0530

    Update Main.java

--- src/main/java/jenkins-pipeline/Main.java
+++ src/main/java/jenkins-pipeline/Main.java
@@ -1,19 +1,13 @@
 package jenkins-pipeline;
 
+public class Main {
 
-class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int n = 10, firstTerm = 0, secondTerm = 1;
-    System.out.println("Fibonacci Series till " + n + " terms:");
-
-    for (int i = 1; i <= n; ++i) {
-      System.out.print(firstTerm + ", ");
-
-      // compute the next term
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
     }
-  }
 }
